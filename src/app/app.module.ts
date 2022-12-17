0136a0e6d9bfdefa7bb4d4f581811bb8ef4baad5
karanimaina <karani.maina2010@gmail.com>
2022-12-17 08:49:12 +0300

    add cart component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
+import { CartComponent } from './components/cart/cart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavigationComponent
+    NavigationComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
